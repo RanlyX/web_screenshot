7b37575806f3eb8f00278bf171376614e7bd01f6
Ranly <et82122@gmail.com>
2020-09-19 20:09:41 +0800

    Refined setup process.

--- setup.sh
+++ setup.sh
@@ -1,38 +1,123 @@
 #!/bin/bash
 
-# install wget, unzip
-sudo apt-get update
-sudo apt-get install -y wget
-sudo apt-get install -y unzip
+# Be used to check command exist.
+# checkWget=$(command -v wget)
+# checkUnzip=$(command -v unzip)
+# checkGC=$(command -v google-chrome)
+# checkPython=$(command -v python)
+# checkPip=$(command -v pip)
 
-# install google chrome
-wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get install
-sudo apt-get install -f
+# print install message.
+echoInstallMsg() {
+    local commandName=$1
+    local msg=$2
+    if [ ${#commandName} -lt 8 ]; then
+        echo -e $commandName'\t\t'$msg>&2
+    else
+        echo -e $commandName'\t'$msg>&2
+    fi
+}
 
-# install pip
-wget https://raw.githubusercontent.com/pypa/get-pip/master/get-pip.py
-sudo python get-pip.py
+# check command be installed.
+checkInstall() {
+    local commandName=$1   # command name
+    # "command -v $commandName >/dev/null" is used to check command exist.
+    # if exist, commandExist will be not null
+    if $(command -v $commandName >/dev/null); then
+        echoInstallMsg $commandName '...installed.'
+        exist=1 # 1 equal false.
+    else
+        echoInstallMsg $commandName '...not install.'
+        exist=0 # 0 equal true.
+    fi
+    # echo $exist
+    return $exist
+}
 
-# install selenium
-sudo pip install -U selenium
+# install wget, unzip, google-chrome and pip
+installCommand() {
+    # start check
+    $(checkInstall "wget")
+    installedWget=$?
+    $(checkInstall "unzip")
+    installedUnzip=$?
+    $(checkInstall "google-chrome")
+    installedGC=$?
+    $(checkInstall "python")
+    installedPython=$?
+    $(checkInstall "pip")
+    installedPip=$?
+    # installedWget=$(checkInstall "wget")
+    # $(checkInstall "unzip")
+    # installedUnzip=$?
+    # $(checkInstall "google-chrome")
+    # installedGC=$?
+    # $(checkInstall "python")
+    # installedPython=$?
+    # $(checkInstall "pip")
+    # installedPip=$?
+    # $(checkInstall "123")
+    # installedWget=$?
+    # echo $installedWget
+    # echo $installedWget
+    # echo $installedUnzip
+    # echo $installedGC
+    # echo $installedPython
+    # echo $installedPip
+    # test=0
+    # echo $installedWget
+    # if $(checkInstall "123"); then
+    #     echo "True"
+    # else
+    #     echo "False"
+    # fi
+    # exit
+    # if not install
+    if [ $installedWget == 0  ] || [ installedUnzip == 0 ] || [ installedGC == 0 ] || [ installedPython == 0 ] || [ installedPip == 0 ]; then
+        # update
+        sudo apt-get update
+        if [ $installedWget == 0 ]; then
+            sudo apt-get install -y wget
+        fi
+        if [ $installedUnzip == 0 ]; then
+            sudo apt-get install -y unzip
+        fi
+        if [ $installedGC == 0 ]; then
+            wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+            sudo dpkg -i google-chrome-stable_current_amd64.deb
+            sudo apt-get install
+            sudo apt-get install -f
+            rm -f google-chrome-stable_current_amd64.deb
+        fi
+        if [ $installedPython == 0 ]; then
+            sudo apt-get install -y python
+        fi
+        if [ $installedPip == 0 ]; then
+            wget https://raw.githubusercontent.com/pypa/get-pip/master/get-pip.py
+            sudo python get-pip.py
+            rm -f get-pip.py
+        # else
+            # python -m pip install --upgrade pip
+        fi
+    else
+        echo "wget, unzip, google-chrome and pip installed already."
+    fi
+}
 
-# system is x86 or x64
-a=$(uname -m)
-if [ $a == i686 ]; then 
-    b=32; 
-    # echo "x32"
-elif [ $a == x86_64 ]; then 
-    b=64; 
-    # echo "x64"
+installCommand
+
+# # install selenium
+checkSelenium=$(pip list 2>/dev/null | grep "selenium")
+if [ -z "$checkSelenium" ]; then
+    sudo pip install -U selenium
 fi
 
-# Get chrome version
+# # Get chrome version
 v=$(google-chrome --version | sed "s/Google Chrome //g" | sed "s/\..*//g")
 ver=$((v))
 
-# Set chromedriver version
+
+# # Set chromedriver version
 if [ $ver -le 70 ] && [ $ver -ge 68 ]; then
     current_version="2.42"
 elif [ $ver -le 67 ] && [ $ver -ge 69 ]; then
@@ -87,10 +172,34 @@ elif [ $ver == 85 ]; then
     current_version="85.0.4183.87"
 else
     current_version=$ver
+    echo 'Unknown version '$ver', please go to http://chromedriver.storage.googleapis.com/ download manual.'
 fi
-# echo $current_version
 
-# Get chromedriver
-wget -O /tmp/chromedriver/chromedriver.zip 'http://chromedriver.storage.googleapis.com/'$current_version'/chromedriver_linux'$b'.zip' 
-unzip /tmp/chromedriver/chromedriver.zip chromedriver -d '.'
-chmod +x chromedriver
+if [ $ver -lt 70 ]; then
+    checkCDV=$current_version
+else
+    checkCDV=$ver
+fi
+# echo $checkCDV
+
+# echo $(chromedriver --version | grep -o $checkCDV)
+
+if [ ! -e "./chromedriver" ]; then
+    echo "Google Chrome version: "$v
+    # system is x86 or x64
+    a=$(uname -m)
+    if [ $a == i686 ]; then 
+        b=32; 
+        echo "This system is 32 bits."
+    elif [ $a == x86_64 ]; then 
+        b=64; 
+        echo "This system is 64 bits."
+    fi
+
+    # Get chromedriver
+    chromeURL='http://chromedriver.storage.googleapis.com/'$current_version'/chromedriver_linux'$b'.zip' 
+    echo 'Downloading chromedriver form '$chromeURL
+    wget -O chromedriver.zip 'http://chromedriver.storage.googleapis.com/'$current_version'/chromedriver_linux'$b'.zip' 
+    unzip chromedriver.zip chromedriver -d '.'
+    chmod +x chromedriver
+fi
